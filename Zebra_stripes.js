ed639a7c4528e4d229ed655eb728cd82392cc7f7
TanakaArthur-N <83368455+TanakaArthur-N@users.noreply.github.com>
2021-06-17 16:29:47 +0200

    Add files via upload

---
+++ Zebra_stripes.js
@@ -0,0 +1,56 @@
+function formValidation() {
+
+    var uname = document.registration.username;
+    var unumber = document.registration.number;
+    var uemail = document.registration.email;
+    if (allLetter(uname)) {
+        if (allnumeric(unumber)) {
+            if (ValidateEmail(uemail)) {
+
+                alert("Enquiry successful")
+
+            }
+        }
+
+
+    }
+    return false;
+
+}
+
+function allLetter(uname) {
+    var letters = /^[A-Za-z]+$/;
+    if (uname.value.match(letters)) {
+        return true;
+    }
+    else {
+        alert('Username must have alphabet characters only');
+        uname.focus();
+        return false;
+    }
+}
+
+
+function allnumeric(unumber) {
+    var numbers = /^[0-9]+$/;
+    if (unumber.value.match(numbers)) {
+        return true;
+    }
+    else {
+        alert('Number must have numeric characters only');
+        unumber.focus();
+        return false;
+    }
+}
+function ValidateEmail(uemail) {
+    var mailformat = /^\w+([\.-]?\w+)*@\w+([\.-]?\w+)*(\.\w{2,3})+$/;
+    if (uemail.value.match(mailformat)) {
+        return true;
+    }
+    else {
+        alert("You have entered an invalid email address!");
+        uemail.focus();
+        return false;
+    }
+}
+

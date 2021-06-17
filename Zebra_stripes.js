11df60a8aacc94a8b0f9df212f42b5449eb793ac
TanakaArthur-N <83368455+TanakaArthur-N@users.noreply.github.com>
2021-06-17 22:34:49 +0200

    Create Zebra_stripes.js

--- Zebra_stripes.js
+++ Zebra_stripes.js
@@ -1,56 +1,8 @@
-function formValidation() {
+function doValidation() {
 
-    var uname = document.registration.username;
-    var unumber = document.registration.number;
-    var uemail = document.registration.email;
-    if (allLetter(uname)) {
-        if (allnumeric(unumber)) {
-            if (ValidateEmail(uemail)) {
-
-                alert("Enquiry successful")
+    var els =document.getElementsByTagName("tr")
+    for (var i = 1; i < els.length; i += 2) {
+        els[i].style = "background-color:black; color: white"
+    }
 
             }
-        }
-
-
-    }
-    return false;
-
-}
-
-function allLetter(uname) {
-    var letters = /^[A-Za-z]+$/;
-    if (uname.value.match(letters)) {
-        return true;
-    }
-    else {
-        alert('Username must have alphabet characters only');
-        uname.focus();
-        return false;
-    }
-}
-
-
-function allnumeric(unumber) {
-    var numbers = /^[0-9]+$/;
-    if (unumber.value.match(numbers)) {
-        return true;
-    }
-    else {
-        alert('Number must have numeric characters only');
-        unumber.focus();
-        return false;
-    }
-}
-function ValidateEmail(uemail) {
-    var mailformat = /^\w+([\.-]?\w+)*@\w+([\.-]?\w+)*(\.\w{2,3})+$/;
-    if (uemail.value.match(mailformat)) {
-        return true;
-    }
-    else {
-        alert("You have entered an invalid email address!");
-        uemail.focus();
-        return false;
-    }
-}
-
